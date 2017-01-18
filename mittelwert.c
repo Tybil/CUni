806a6e257a1565b532eac77c0001b6dc0669c193
Jonas Noglik <jonas@familie-noglik.de>
2017-01-18 11:03:06 +0100

    Vaariablen  sinnvoll benannt

--- mittelwert.c
+++ mittelwert.c
@@ -2,12 +2,12 @@
  int main(void) {
       int x;
       int z = 0;
-      int mittelwert = 0;
+      int summe = 0;
       while(scanf("%i",&x)==1) {
-          mittelwert = mittelwert + x;
+          summe = summe + x;
           z++;
           }
-      double a =1.0* mittelwert/z; 
+      double a =1.0* summe/z; 
       printf("%f\n",a);
 
  }
