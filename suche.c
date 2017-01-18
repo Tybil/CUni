246eba1b90a03d72d4ee260184449618ce83b5b4
Jonas Noglik <jonas@familie-noglik.de>
2017-01-18 13:30:39 +0100

    optimierung im code

--- suche.c
+++ suche.c
@@ -3,19 +3,17 @@
 bool contains(char const *search, int searchSize, char const *input, int inputSize){
     bool a=false;
     for(int i = 0; i<inputSize;i++){
-        if(input[i]==search[0]){
-            for(int j =1;j<searchSize;j++){
-                if(input[j+i]==search[j]){
-                    bool a = true;
-                } else {
-                    a=false;
-                    break;
-                }
-            }
-            if(a){
-                return true;
+        for(int j =0;j<searchSize;j++){
+            if(input[j+i]==search[j]){
+                bool a = true;
+            } else {
+                a=false;
+                break;
             }
         }
+        if(a){
+            return true;
+        }   
     }
     return false;
 }
