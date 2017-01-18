8f1e47324666fa4b556d331b107e7ef83c25f1a7
Unknown <jonas@familie-noglik.de>
2017-01-18 14:23:04 +0100

    Die contains-Methode wurde Korrigiert und die ausgabe verfollständigt

--- suche.c
+++ suche.c
@@ -7,12 +7,11 @@ bool contains(char const *search, int searchSize, char const *input, int inputSi
     bool a=false;
     for(int i = 0; i<inputSize;i++){
         for(int j =0;j<searchSize;j++){
-            if(input[j+i]==search[j]){
-                bool a = true;
-            } else {
+            if(input[j+i]!=search[j]){
                 a=false;
                 break;
             }
+            a=true;
         }
         if(a){
             return true;
@@ -24,13 +23,11 @@ bool contains(char const *search, int searchSize, char const *input, int inputSi
 int main(int argc, char *argv[]){
     char *s = malloc(1024*sizeof(char));
     int a = 1;
-    while(*s=fgets(s,1024,stdin)!=NULL){
+    while(fgets(s,1024,stdin)!=NULL){
         if(contains(argv[1],strlen(argv[1]),s,strlen(s))){
-            printf("%i",a);
+            printf("Der Suchtext wurde in Zeile %i gefunden!\n Zeile: %s",a,s);
         }
         a++;
     }
-    printf("%i",a);
-
     return 0;
 }
