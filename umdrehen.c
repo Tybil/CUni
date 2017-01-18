5e30eebe6ac6d51f1d9dfc428caf2a8f2fa1e2de
Jonas Noglik <jonas@familie-noglik.de>
2017-01-18 11:55:06 +0100

    umkehren der standarteingabe

---
+++ umdrehen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int *input = malloc(8*sizeof(int));
+    int a = 8;
+    int x;
+    int z=0;
+    while(scanf("%i",&x)==1) {
+        if(z>=a){
+            a = a+8;
+            input = realloc(input,a*sizeof(int));
+        }
+        input[z]=x;
+        z++;
+    }
+    for(int i = z-1;i>=0;i--){
+        printf("%i\n",input[i]);
+    }
+    
+}
