a7d918d91b64111d147213a1af31b752edaa58a4
Jonas Noglik <jonas@familie-noglik.de>
2017-01-18 13:22:11 +0100

    erster versuch einer containsmethode

--- suche.c
+++ suche.c
@@ -1,7 +1,23 @@
 #include <stdbool.h>
 
 bool contains(char const *search, int searchSize, char const *input, int inputSize){
-    //TODO
+    bool a=false;
+    for(int i = 0; i<inputSize;i++){
+        if(input[i]==search[0]){
+            for(int j =1;j<searchSize;j++){
+                if(input[j+i]==search[j]){
+                    bool a = true;
+                } else {
+                    a=false;
+                    break;
+                }
+            }
+            if(a){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 int main(void){
